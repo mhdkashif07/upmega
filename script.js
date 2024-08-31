c4322a35b812e46571c085f481e253db07759208
Muhammad Kashif <60599437+mhdkashif07@users.noreply.github.com>
2024-09-01 01:09:37 +0500

    fix homepage

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
-const navbar = document.querySelectorAll('.navbar-collapse a')
-const dropdownIcon = document.querySelector('.dropdown')
-const logo = document.querySelector('.logo')
-const logob = document.querySelector('.logo-b')
-const icons = document.querySelectorAll('.social-icons img')
-const toggle = document.querySelector('.dropdown-toggle')
-const dropdown = document.querySelector('.navbar-drop-down')
+const navbar = document.querySelectorAll(".navbar-collapse a");
+const dropdownIcon = document.querySelector(".dropdown");
+const logo = document.querySelector(".logo");
+const logob = document.querySelector(".logo-b");
+const icons = document.querySelectorAll(".social-icons img");
+const toggle = document.querySelector(".dropdown-toggle");
+const dropdown = document.querySelector(".navbar-drop-down");
 
-console.log(logo,logob);
+console.log(logo, logob);
 
+dropdownIcon.addEventListener("mouseenter", () => {
+  dropdown.classList.remove("hide");
 
-dropdownIcon.addEventListener('click', () => {
-    dropdown.classList.toggle('hide')
+  logo.classList.toggle("hide");
+  logob.classList.toggle("hide");
 
-    logo.classList.toggle('hide')
-    logob.classList.toggle('hide')
+  navbar.forEach((item) => {
+    console.log(item.innerHTML);
 
-    navbar.forEach((item) => {
-        item.classList.toggle('black')
-        item.style.fontWeight = '600'
-    });
+    item.classList.toggle("black");
+    item.style.fontWeight = "600";
+    if (
+      item.innerHTML == "Home" ||
+      item.innerHTML == "About" ||
+      item.innerHTML == "Contact"
+    ) {
+      item.addEventListener("mouseenter", () => {
+        dropdown.classList.add("hide");
+      });
+    }
+  });
 
-    icons.forEach((item) => {
-        item.classList.toggle('filter-icons')
-    })
-})
+  icons.forEach((item) => {
+    // item.addEventListener('')
+    item.classList.toggle("filter-icons");
+    dropdown.classList.remove("hide");
+  });
+});
+
+console.log(dropdown, "dropdddd");
+
+dropdown.addEventListener("mouseleave", () => {
+  console.log("leaved");
+  dropdown.classList.add("hide");
+  // navbar.forEach((item) => {
+  //   item.classList.remove("black");
+  //   item.style.fontWeight = "";
+  // });
+});
+
+// dropdownIcon.addEventListener("mouseleave", () => {
+//   console.log("leaved");
+//   dropdown.classList.add("hide");
+//   // navbar.forEach((item) => {
+//   //   item.classList.remove("black");
+//   //   item.style.fontWeight = "";
+//   // });
+// });
